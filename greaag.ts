dbb0167a06b55c6d57a9a4230a5a7226fa17bb06
孔科翰 <kongkehan@jd.com>
2019-12-17 14:57:49 +0800

    test

--- greaag.ts
+++ greaag.ts
@@ -1,5 +1,24 @@
-function great(person: string): string {
-    return 'hello ' + person
+class User {
+    fullName: string
+    firstName: string
+    lastName: string
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.fullName = firstName + ' ' + lastName
+    }
 }
-let myname = 'kkh'
-console.log(great(myname))
+
+interface Person {
+    firstName: string,
+    lastName: string
+}
+
+function great(person: Person): string {
+    return 'hello ' + person.firstName + person.lastName
+}
+
+let user = new User('kkk', 'hhh')
+
+console.log(great(user))
---
+++ type.ts
@@ -0,0 +1,20 @@
+let flag: boolean = true
+
+let decLiteral: number = 20
+let hexLiteral: number = 0x14
+let bingryLiteral: number = 0b10100
+let octalLiteral: number = 0o24
+
+let myname: string = 'kkh'
+let uname: string = `my name is ${decLiteral} ahahahahha`
+
+let list: number[] = [1, 2, 3, 4, 5]
+let list2: Array<number> = [1, 2, 3]
+
+let x: [string, number]
+x = ['hello', 10]
+
+
+
+
+
